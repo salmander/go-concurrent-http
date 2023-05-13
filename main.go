a7860210e49186968d1b3a43b2f88812a09cc335
Salman Ahmed <salman.great@gmail.com>
2023-05-13 14:47:40 +0100

    changed the concurrent function to make a http call to a weather api instead

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
 
+const URL = "https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&current_weather=true"
+
 func main() {
-	http.HandleFunc("/my-endpoint", MyHandler)
+	http.HandleFunc("/", MyHandler)
 
 	// Start the HTTP server
 	fmt.Println("Server listening on port 8080")
@@ -22,11 +27,35 @@ func MyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func MyConcurrentFunction() {
-	// Perform background tasks or computations here
-	fmt.Println("concurrent function doing task 1", time.Now().Format(time.RFC822))
-	time.Sleep(1 * time.Second)
-
-	fmt.Println("concurrent function doing task 2", time.Now().Format(time.RFC822))
+	log.Println("Making a http request to get current weather information", time.Now().Format(time.RFC822))
 	time.Sleep(5 * time.Second)
-	fmt.Println("concurrent function all done", time.Now().Format(time.RFC822))
+	// Make an HTTP GET request to the API
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Println("failed to make request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("failed to read response body:", err)
+		return
+	}
+
+	// Unmarshal the response JSON into the Response struct
+	var response struct {
+		CurrentWeather struct {
+			Temperature float64 `json:"temperature"`
+		} `json:"current_weather"`
+	}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Println("failed to unmarshal response:", err)
+		return
+	}
+
+	// Log the temperature
+	log.Println("Temperature:", response.CurrentWeather.Temperature)
 }
